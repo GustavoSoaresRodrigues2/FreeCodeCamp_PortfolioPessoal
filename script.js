70e9ef7ef08cb657d13b971beef6bf6cf727f4c6
unknown <you@example.com>
2023-07-07 16:09:20 -0300

    arrumandoHeaderComCHK

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mudança de Branco para Cinza
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+// ///Mudança de Branco para Cinza
